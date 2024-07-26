f9221740ebcbe2eabf5502e7044385a3f8c25281
MIXXI <hamid.oct21@gmail.com>
2024-07-26 22:05:03 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["0x78a3925275302ffb6d17b20b43e542e53f7848ae17849de015fe93b83d3f4b04"],
+      accounts: ["0x78"],
     },
   },
 };
